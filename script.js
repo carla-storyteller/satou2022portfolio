90ea3abaef89d9f77a10925425c554b7e670c260
carla-storyteller <48150803+carla-storyteller@users.noreply.github.com>
2021-06-19 19:51:43 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).on('ready', function() {
+  $(".regular").slick({
+  	centerMode: true,
+    centerPadding: '5%',
+  	dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+  });
+});
